9024d7c526614847e3a48bc8653898f1f88165c4
shahzaib <shahzaibalam231@gmail.com>
2022-03-01 16:42:51 +0500

    builtin fake db

--- src/main/java/com/example/securitypract/Security/SecurityConfig.java
+++ src/main/java/com/example/securitypract/Security/SecurityConfig.java
@@ -1,29 +1,29 @@
 package com.example.securitypract.Security;
 
 
+import com.example.securitypract.Auth.ApplicaitonUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder ;
+    private final ApplicaitonUserService applicaitonUserService ;
 
     @Autowired
-    SecurityConfig(PasswordEncoder passwordEncoder){
+    SecurityConfig(PasswordEncoder passwordEncoder, ApplicaitonUserService applicaitonUserService){
         this.passwordEncoder = passwordEncoder;
+        this.applicaitonUserService = applicaitonUserService;
     }
 
 
@@ -47,40 +47,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    //  creating user
+//    providing our service details
+
+
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
     @Bean
-    protected UserDetailsService userDetailsService() {
-        System.out.println(ApplicationUserRule.ADMIN.getPermissions());
-         UserDetails  shahzaib = User.builder()
-                .username("shahzaib")
-                .password(passwordEncoder.encode("123"))
-//                .roles(ApplicationUserRule.STUDENT.name())
-                 //adding custom made authorities
-                 .authorities(ApplicationUserRule.STUDENT.getGrantedAuthorities())
-
-                 .build();
-
-        UserDetails  ali = User.builder()
-                .username("ali")
-                .password(passwordEncoder.encode("123"))
-//                .roles(ApplicationUserRule.ADMIN.name())
-                //adding custom made authorities
-                .authorities(ApplicationUserRule.ADMIN.getGrantedAuthorities())
-                .build();
-        System.out.println(ApplicationUserRule.ADMIN.getGrantedAuthorities());
-        UserDetails  aqib = User.builder()
-                .username("aqib")
-                .password(passwordEncoder.encode("123"))
-//                .roles(ApplicationUserRule.TRAINEE.name())
-                //adding custom made authorities
-                .authorities(ApplicationUserRule.TRAINEE.getGrantedAuthorities())
-                .build();
-
-        return new InMemoryUserDetailsManager(
-                shahzaib,
-                ali,
-                aqib
-        );
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider  = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(this.passwordEncoder);
+        provider.setUserDetailsService(this.applicaitonUserService);
+        return provider ;
     }
 }
